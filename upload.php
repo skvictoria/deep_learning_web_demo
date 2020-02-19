e8b040c47aa3deb1ab651e49cce7af3234b65898
skvictoria <skvictoria.sk@gmail.com>
2020-02-19 19:32:50 +0900

    link with php

---
+++ upload.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<?php
+
+echo "file name : ",$_FILES["input_img"]["name"], "<BR>";
+echo "mime type : ",$_FILES["input_img"]["type"], "<BR>";
+echo "file size : ",$_FILES["input_img"]["size"], "<BR>";
+echo "temp name : ",$_FILES["input_img"]["tmp_name"], "<BR>";
+echo "error cod : ",$_FILES["input_img"]["error"], "<BR>";
+
+
+$filename = "./pictures/".$_FILES["input_img"]["name"];
+if($_FILES["input_img"]["size"]==0){
+	echo "cannot upload file";
+}else{
+	if(move_uploaded_file($_FILES["input_img"]["tmp_name"],$filename) == true){
+		echo "upload finish";
+	
+	}else{
+		echo "upload failed", "<BR>";
+		print_r($_FILES);
+	}
+}
+
+exec('python3 test.py 2>&1', $out, $status);
+echo $out[0];
+
+?>
+</body>
+</html>
